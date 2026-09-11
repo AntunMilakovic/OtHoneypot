6ff22c4fc4ab9b86da7a37e6d5d5064bd9a5627c
AntunMilakovic <106969056+AntunMilakovic@users.noreply.github.com>
2026-09-11 14:43:20 +0200

    Remove unused Modbus register value property

--- OtHoneypot.Core/OtHoneypot.Protocols/Modbus/ModbusRegister.cs
+++ OtHoneypot.Core/OtHoneypot.Protocols/Modbus/ModbusRegister.cs
@@ -2,17 +2,10 @@ namespace OtHoneypot.Core.Protocols;
 
 public class ModbusRegister
 {
-    public string Name { get; set; }
-
+    public string Name { get; set; } = string.Empty;
     public ModbusRegisterType Type { get; set; }
-
     public ushort Address { get; set; }
-
     public ModbusDataType DataType { get; set; }
-
-    public object Value { get; set; }
-
     public bool Writable { get; set; }
-
     public int DataTemplateId { get; set; }
 }
