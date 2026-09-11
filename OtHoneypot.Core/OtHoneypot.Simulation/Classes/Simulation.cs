35144d671365d7a9530df68096fe3685c476d189
AntunMilakovic <106969056+AntunMilakovic@users.noreply.github.com>
2026-09-11 14:43:08 +0200

    Remove dead simulation constructor code

--- OtHoneypot.Core/OtHoneypot.Simulation/Classes/Simulation.cs
+++ OtHoneypot.Core/OtHoneypot.Simulation/Classes/Simulation.cs
@@ -11,18 +11,8 @@ public class Simulation : ISimulation
     /// <summary>
     /// Gets or sets the refresh rate in seconds for the data template.
     /// </summary>
-    public int RefreshRate { get; set; }  
+    public int RefreshRate { get; set; }
 
     public float? ValueChangeRate { get; set; }
     public SimulationType Type { get; set; }
-
-    // public Simulation(SimulationType type, float minValue, float maxValue, int refreshRate, float? valueChangeRate = null)
-    // {
-    //     Type = type;
-    //     MinValue = minValue;
-    //     MaxValue = maxValue;
-    //     RefreshRate = refreshRate;
-    //     ValueChangeRate = valueChangeRate;
-    // }
-    
 }
