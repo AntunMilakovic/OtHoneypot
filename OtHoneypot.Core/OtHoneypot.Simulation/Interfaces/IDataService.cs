e7d85a2ae9d25293584c9177e5ce4eeacdb8ac7b
AntunMilakovic <106969056+AntunMilakovic@users.noreply.github.com>
2026-09-11 14:23:50 +0200

    Expose simulation command API

--- OtHoneypot.Core/OtHoneypot.Simulation/Interfaces/IDataService.cs
+++ OtHoneypot.Core/OtHoneypot.Simulation/Interfaces/IDataService.cs
@@ -1,17 +1,15 @@
-using System;
 using System.Collections.Generic;
-using Microsoft.VisualBasic;
+using OtHoneypot.Core.Enums;
 
 namespace OtHoneypot.Core.Interfaces;
+
 public interface IDataService
 {
-    /// <summary>
-    /// Gets the generated data.
-    /// </summary>
-    /// <returns>List of generated data items</returns>
     List<IData> GetGeneratedDatas(List<int> dataTemplateIds);
     List<IData> GetGeneratedDatas(List<string> dataTemplateNames);
 
     bool GetGeneratedData(int dataTemplateId, out IData data);
     bool GetGeneratedData(string dataTemplateName, out IData data);
+
+    bool ExecuteSimulationCommand(int dataTemplateId, SimulationCommand command);
 }
