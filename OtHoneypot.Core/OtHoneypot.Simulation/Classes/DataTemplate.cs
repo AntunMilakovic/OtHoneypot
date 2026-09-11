cfe50b0845e8d65fa28f99e4377df3b2087a7286
AntunMilakovic <106969056+AntunMilakovic@users.noreply.github.com>
2026-09-11 14:43:03 +0200

    Clean up data template configuration model

--- OtHoneypot.Core/OtHoneypot.Simulation/Classes/DataTemplate.cs
+++ OtHoneypot.Core/OtHoneypot.Simulation/Classes/DataTemplate.cs
@@ -1,4 +1,3 @@
-using OtHoneypot.Core.Enums;
 using OtHoneypot.Core.Interfaces;
 
 namespace OtHoneypot.Core.Data;
@@ -6,18 +5,8 @@ namespace OtHoneypot.Core.Data;
 public class DataTemplate : IDataTemplate
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string DataType { get; set; }
-
-    public Simulation Simulation { get; set; }
-
-    // public DataTemplate(int id, string name, string description, string dataType, float minValue, float maxValue, int refreshRate, SimulationType simulationType, float? valueChangeRate = null)
-    // {
-    //     Id = id;
-    //     Name = name;
-    //     Description = description;
-    //     DataType = dataType;
-    //     Simulation = new Simulation(simulationType, minValue, maxValue, refreshRate, valueChangeRate);
-    // }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string DataType { get; set; } = string.Empty;
+    public Simulation Simulation { get; set; } = new();
 }
