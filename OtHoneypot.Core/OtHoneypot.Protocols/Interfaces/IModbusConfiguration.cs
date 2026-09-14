38dd77378dc8e6bd8aea173f689981be78c24b4c
AntunMilakovic <106969056+AntunMilakovic@users.noreply.github.com>
2026-09-14 09:28:22 +0200

    Expose Modbus slave UnitId in configuration interface

--- OtHoneypot.Core/OtHoneypot.Protocols/Interfaces/IModbusConfiguration.cs
+++ OtHoneypot.Core/OtHoneypot.Protocols/Interfaces/IModbusConfiguration.cs
@@ -5,6 +5,7 @@ namespace OtHoneypot.Core.Interfaces;
 
 public interface IModbusConfiguration : IProtocolConfiguration
 {
+    byte UnitId { get; set; }
     List<ModbusDevice> Devices { get; set; }
     List<ModbusRegister> Registers { get; set; }
     List<ModbusCommandMapping> CommandMappings { get; set; }
